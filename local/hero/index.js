b64ad8fcd95b1eb603781ddc535cccf2761d6f0d
Enrico Marino <enrico.marino@email.com>
2012-12-07 12:01:07 -0800

    hero component

--- local/hero/index.js
+++ local/hero/index.js
@@ -1,13 +1,21 @@
+var domify = require('domify');
 var View = require('backbone-view');
 var template = require('./template');
 
 module.exports = View.extend({
 
   initialize: function (options) {
+    this.setElement(domify(template));
     return this;
   },
 
-  render: function () {
+  render: function (options) {
+    var options = options || {};
+    var title = options.title || '';
+    var subtitle = options.subtitle || '';
+    var el = this.$el;
+    el.find('h1').text(title);
+    el.find('h2').text(subtitle);
     return this;
   },
 
