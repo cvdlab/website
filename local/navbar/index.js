9b898490a5e9aa4712fae879a2103a33ccc7b3ff
Enrico Marino <enrico.marino@email.com>
2012-12-07 10:17:59 -0800

    navbar

--- local/navbar/index.js
+++ local/navbar/index.js
@@ -1,9 +1,12 @@
+var $ = require('jquery');
+var domify = require('domify');
 var View = require('backbone-view');
 var template = require('./template');
 
 module.exports = View.extend({
 
   initialize: function (options) {
+    this.setElement(domify(template));
     return this;
   },
 
@@ -11,6 +14,27 @@ module.exports = View.extend({
     return this;
   },
 
-  events: {}
+  activate: function (item) {
+    $(item).addClass('active');
+    return this;
+  },
+
+  deactivate: function (item) {
+    $(item).removeClass('active');
+    return this;
+  },
+
+  deactivate_all: function () {
+    $('li').removeClass('active');
+    return this;
+  },
+
+  events: {
+    'click li': 'on_click_item'
+  },
+
+  on_click_item: function (event) {
+    this.deactivate_all().activate(event.currentTarget);
+  }
 
 });
